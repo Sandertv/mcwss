f0354d52b283bb85252583d682c9bf5635747594
Sander <st.ten.veldhuis@gmail.com>
2019-03-23 11:29:50 +0100

    StartWorld event: Added missing Host key.

--- protocol/event/start_world.go
+++ protocol/event/start_world.go
@@ -2,5 +2,7 @@ package event
 
 // StartWorld is sent by the client when it starts a world by clicking it in the main menu.
 type StartWorld struct {
-	// StartWorld has no fields.
+	// Host is the host IP address of the world. For local worlds, this field is not set and is empty. For
+	// servers, this is the IP address of the server.
+	Host string `json:",omitempty"`
 }
